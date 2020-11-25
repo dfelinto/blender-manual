c69a0620fc4db91e357a3d57b3f3e41a4349185b
Blendify <Blendify@c4de1f47-6596-e411-a384-0024e86c2797>
2020-11-25 18:29:59 +0000

    Bump version to 2.92
    git-svn-id: https://svn.blender.org/svnroot/bf-manual/trunk/blender_docs@7422 c4de1f47-6596-e411-a384-0024e86c2797

--- manual/conf.py
+++ manual/conf.py
@@ -23,7 +23,7 @@ import os
 # Not used directly by Sphinx, only used by this file.
 #
 
-blender_version = '2.91'
+blender_version = '2.92'
 
 
 # If extensions (or modules to document with autodoc) are in another directory,
