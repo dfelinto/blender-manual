d296d5d90f6b50199633c96172a603e953affde2
Blendify <Blendify@c4de1f47-6596-e411-a384-0024e86c2797>
2021-02-03 17:57:55 +0000

    Merge branch 'blender-2.92-release'
    git-svn-id: https://svn.blender.org/svnroot/bf-manual/trunk/blender_docs@7711 c4de1f47-6596-e411-a384-0024e86c2797

--- resources/theme/js/version_switch.js
+++ resources/theme/js/version_switch.js
@@ -19,7 +19,7 @@ var all_langs = {
 	"pt": "Portugu&ecirc;s",
 	"ru": "&#x3A1;&#x443;&#x441;&#x441;&#x43A;&#x438;&#x439;",
 	"sk": "Slov&aacute;k",
-	"sl": "Sloven&#353;&#269;ina"
+	"sl": "Sloven&#353;&#269;ina",
 	"sr": "&#1089;&#1088;&#1087;&#1089;&#1082;&#1080;",
 	"uk": "Ukra&#1111;na",
 	"vi": "Ti&#x1EBF;ng Vi&#x1EC7;t",
