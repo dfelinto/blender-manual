b1f666c22a3a7949a8a30a76cf3e9927876b61ff
Blendify <Blendify@c4de1f47-6596-e411-a384-0024e86c2797>
2021-08-20 17:26:53 +0000

    Config: Disable copy sources
    This is no longer needed now that we link to a only copy of the sources.
    
    In previous versions of sphinx this was still needed to control search
    summeries but that is no longer needed after the recent update.
    
    Note however sphinx 4.1.2 reverted this which breaks search summaries.
    sphinx 4.2 should fix this so we will be skipping the rest of 4.1.x versions.
    
    See: https://github.com/sphinx-doc/sphinx/issues/9456
    
    
    git-svn-id: https://svn.blender.org/svnroot/bf-manual/trunk/blender_docs@8334 c4de1f47-6596-e411-a384-0024e86c2797

--- manual/conf.py
+++ manual/conf.py
@@ -259,7 +259,7 @@ html_additional_pages = {
 
 # If true, links to the reST sources are added to the pages.
 html_show_sourcelink = False
-html_copy_source = True
+html_copy_source = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 html_show_sphinx = False
