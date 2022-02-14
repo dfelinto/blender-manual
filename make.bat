2f8182a823c8f1bd447a3ace67b0d46f61614794
Mazhuka <Mazhuka@c4de1f47-6596-e411-a384-0024e86c2797>
2022-02-14 16:57:20 +0000

    fix reference
    git-svn-id: https://svn.blender.org/svnroot/bf-manual/trunk/blender_docs@8967 c4de1f47-6596-e411-a384-0024e86c2797

--- make.bat
+++ make.bat
@@ -7,7 +7,7 @@ if "%SPHINXBUILD%" == "" (
 )
 set SOURCEDIR=.\manual
 set BUILDDIR=build
-if "%BF_LANG%" == "" set BF_LANG=es
+if "%BF_LANG%" == "" set BF_LANG=en
 set SPHINXOPTS=-j auto -D language=%BF_LANG%
 
 REM Check if sphinx-build is available and fallback to Python version if any
